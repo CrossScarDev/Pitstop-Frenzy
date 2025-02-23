8ef51679af7602a4931350e6102dd21bda6ec338
CrossScar <crossscar@proton.me>
2025-02-23 01:20:53 -0800

    allow return to main menu

--- scripts/main.gd
+++ scripts/main.gd
@@ -146,3 +146,7 @@ func _explosion_animation_finished() -> void:
 
 func _on_play_again_pressed() -> void:
 	get_tree().reload_current_scene()
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/title.tscn")
