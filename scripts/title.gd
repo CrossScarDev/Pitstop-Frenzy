37f1dc9c871c3c509bdf72704502c5fdf21fe684
CrossScar <crossscar@proton.me>
2025-02-22 20:29:37 -0800

    allow pressing enter on title screen to start game

--- scripts/title.gd
+++ scripts/title.gd
@@ -1,5 +1,9 @@
 extends Control
 
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Next Car"):
+		get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
 func _on_go_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
